3b2b5fd8953c1c1b79c7656949a2aba8b8e36cdd
joaquin <jecorrea@protonmail.com>
2021-12-16 22:13:38 -0500

    Added functionality to create tags

--- script.js
+++ script.js
@@ -10,6 +10,25 @@ textarea.addEventListener("keyup", (event) => {
 
 function createTags(input) {
   //Split the input by a comma and then put them into an array
-  const tags = input.split(",");
-  console.log(tags);
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  //   console.log(tags);
+
+  // Create the tags
+  tagsEl.innerHTML = "";
+
+  /* Now to create span elements for the tags
+        <span class="tag">Choice 1</span>
+        <span class="tag">Choice 2</span>
+        <span class="tag">Choice 3</span>
+  */
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
 }
